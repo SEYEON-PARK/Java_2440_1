0191db4668928152f6ae7f64583806ec6c93a1d2
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-08-23 00:08:26 +0900

    Java언어 BAEKJOON 2440번 문제 풀기!(1)

--- Java_2440_1.java
+++ Java_2440_1.java
@@ -10,12 +10,12 @@ import java.util.*; // 스캐너를 사용하기 위해서
 
 class Main{ // Main 클래스
     public static void main(String [] args){ // main 메소드
-        Scanner a=new Scanner(System.in);
-        int N=a.nextInt();
+        Scanner a=new Scanner(System.in); // 새로운 스캐너 a 생성하기
+        int N=a.nextInt(); // 사용자로부터 정수 입력받기
         
-        for (int i=N;i>0;i--){
-            for (int j=i;j>0;j--){
-                System.out.print("*");
+        for (int i=N;i>0;i--){ // i가 N부터 0보다 클 때까지 1씩 감소하며 반복
+            for (int j=i;j>0;j--){ // j가 i부터 0보다 클 때까지 1씩 감소하며 반복
+                System.out.print("*"); // 별 출력하기
             }
             System.out.println();
         }
